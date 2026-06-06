f269a99f142def337aea6bae80a6c06c4099d575
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-06-06 21:47:07 +0700

    fix(users): fix missing password hashing on registration

--- src/main/java/com/qiraht/ppob_sims_spring/service/UserService.java
+++ src/main/java/com/qiraht/ppob_sims_spring/service/UserService.java
@@ -5,16 +5,19 @@ import com.qiraht.ppob_sims_spring.entity.User;
 import com.qiraht.ppob_sims_spring.enums.UserStatus;
 import com.qiraht.ppob_sims_spring.exception.custom.ValidationException;
 import com.qiraht.ppob_sims_spring.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final UserWalletService userWalletService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserWalletService userWalletService) {
+    public UserService(UserRepository userRepository, UserWalletService userWalletService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userWalletService = userWalletService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void registerUser(RegisterRequest request) {
@@ -22,9 +25,11 @@ public class UserService {
             throw new ValidationException("Email sudah terdaftar");
         }
 
+        String hashedPassword = passwordEncoder.encode(request.password());
+
         User user = User.builder()
                 .email(request.email())
-                .password(request.password())
+                .password(hashedPassword)
                 .firstName(request.first_name())
                 .lastName(request.last_name())
                 .status(UserStatus.ACTIVE)
