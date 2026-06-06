437e681fff0eb502747cc1e4695edbce137bf98a
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-06-06 10:51:51 +0700

    first commit

---
+++ src/main/java/com/qiraht/ppob_sims_spring/PpobSimsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.qiraht.ppob_sims_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpobSimsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpobSimsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qiraht/ppob_sims_spring/PpobSimsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiraht.ppob_sims_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpobSimsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
