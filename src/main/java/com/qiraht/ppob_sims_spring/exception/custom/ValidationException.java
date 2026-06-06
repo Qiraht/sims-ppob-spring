63460605c3ee636ba0227a62090d12c724796b22
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-06-06 17:06:47 +0700

    feat(errors): add validation exception
    - add custom exception for manual validation
    - add custom exception for jakarta validation
    - implements custom exceptions on GlobalExceptionHandler

---
+++ src/main/java/com/qiraht/ppob_sims_spring/exception/custom/ValidationException.java
@@ -0,0 +1,7 @@
+package com.qiraht.ppob_sims_spring.exception.custom;
+
+public class ValidationException extends BusinessException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/qiraht/ppob_sims_spring/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/qiraht/ppob_sims_spring/exception/handler/GlobalExceptionHandler.java
@@ -1,14 +1,39 @@
 package com.qiraht.ppob_sims_spring.exception.handler;
 
 import com.qiraht.ppob_sims_spring.dto.ApiResponse;
+import com.qiraht.ppob_sims_spring.exception.custom.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(ValidationException ex, HttpServletRequest request) {
+        ApiResponse<Void> body = ApiResponse.error(102, ex.getMessage());
+
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("Validation Failed");
+
+        ApiResponse<?> body = ApiResponse.error(102, message);
+
+        return ResponseEntity.badRequest().body(body);
+    }
+
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleBusinessException(Exception ex, HttpServletRequest request) {
         ApiResponse<Void> body = ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error");
